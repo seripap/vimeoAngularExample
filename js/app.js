e9a325fbe8434c0bf73a5ddfc6370d5516e9876e
Daniel Seripap <daniel.seripap@weather.com>
2014-03-01 12:30:10 -0500

    Made circles

--- js/app.js
+++ js/app.js
@@ -1,3 +1,3 @@
 /*! vimeoExample
- @modified Saturday, March 1st, 2014, 8:37:49 AM */
+ @modified Saturday, March 1st, 2014, 12:22:16 PM */
 var vimeoApp=angular.module("vimeoApp",["angularSmoothscroll"]);
--- js/videoMetadata.controller.js
+++ js/videoMetadata.controller.js
@@ -1,3 +1,3 @@
 /*! vimeoExample
- @modified Saturday, March 1st, 2014, 2:27:04 AM */
+ @modified Saturday, March 1st, 2014, 12:22:16 PM */
 vimeoApp.controller("videoMetadataController",function($scope,$sce){$scope.$on("videoCollectionChanged",function(event,data){$scope.metadata=data}),$scope.safeHtml=function(html){return $sce.trustAsHtml(html)}});
